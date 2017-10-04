67a77343eeaa7c0ea2fb049b82ace04314958df1
Zachary Williams <z.williams@baypath.net>
2017-10-04 10:39:32 -0400

    Just some more tests. ZW

--- src/linkedlistreview/LinkedListReview.java
+++ src/linkedlistreview/LinkedListReview.java
@@ -5,7 +5,7 @@ public class LinkedListReview {
        LinkedList a = new LinkedList();
        a.addFirst("Reeeee");
        a.addFirst("Pepe");
-       System.out.println(a.getFirst());
+       System.out.println(a.getFirst().getValue());
        System.out.println(a);
     } 
 }
