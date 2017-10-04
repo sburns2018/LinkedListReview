aa5b36853d3adebc29f334d2fb8e1e918a8f31b0
Zachary Williams <z.williams@baypath.net>
2017-10-04 10:15:58 -0400

    Basic Node Functionality ZW

--- src/linkedlistreview/Node.java
+++ src/linkedlistreview/Node.java
@@ -4,11 +4,19 @@ public class Node {
     private Node next;
     private Node previous;
     
-    public Node() { }
+    public Node(Node nextus, Node previousus) { next = nextus; previous = previousus; }
     
     public Node getNext() { return next; }
     
     public Node getPrevious() { return previous; }
+    
+    public void setNext(Node nextus) {
+        next = nextus;
+    }
+    
+    public void setPrevious(Node previousus) {
+        previous = previousus;
+    }
     
     public int compareTo() { return 1; }
 }
